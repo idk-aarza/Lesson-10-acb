8492365c0b475d707803ee8a919c81270aa46428
idk-aarza <aarzak2012@gmail.com>
2024-11-08 12:26:31 +0530

    gdgps

---
+++ gdgps.py
@@ -0,0 +1,14 @@
+k=int(input("Enter a no."))
+print(k)
+i=int(input("Enter a power till 5"))
+print(i)
+if i==2:
+    print(k*k)
+elif i==3:
+    print(k*k*k)
+elif i==4:
+    print(k*k*k*k)
+elif i==5:
+    print(k*k*k*k*k)
+else:
+    print('invalid power')
